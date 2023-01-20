e76fc1ff23e73a6b9afad1e2771e3c0c3605e63b
harshalgawas08 <115731489+harshalgawas08@users.noreply.github.com>
2023-01-20 12:39:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navkinks = document.querySelector('.nav-links')
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+menuBtn.addEventListener('click', () => {
+  navkinks.classList.toggle('mobile-menu')
+})
+
+let movies = [
+    {
+      name: "falcon and the winter soldier",
+      des:
+        "The Falcon and the Winter Soldier is an American television miniseries created by Malcolm Spellman for the streaming service Disney+, based on Marvel Comics .",
+      image: " images/slider 2.PNG"
+    },
+    {
+      name: "Loki",
+      des:
+        "Loki became the first Disney+ show from Marvel Studios to be officially confirmed for a Season 2 in its sixth episode’s post-credits scene, although details are scarce beyond that news.",
+      image: "images/slider 1.PNG"
+    },
+    {
+      name: "Wanda Vision",
+      des:
+        "Wanda basically has a mental breakdown that results in the death of her husband Vision, Hawkeye and a bunch of other people.And they all live together.",
+      image: "images/slider 3.PNG"
+    },
+    {
+      name: "raya and the last night",
+      des:
+        "Raya and the Last Dragon is a fantasy film set in the fictional land of Kumandra, but that world is inspired by the beautiful cultures of Southeast Asia.",
+      image: "images/slider 4.PNG"
+    },
+    {
+      name: "luca",
+      des:
+        "The movie follows Luca Alberto and Giulias story of friendship and adventure in the Italian Riviera Recently Enrico Casarosa director of Luca revealed how his childhood inspired.",
+      image: "images/slider 5.PNG"
+    }
+  ];
+  
+  const carousel = document.querySelector(".carousel");
+  let sliders = [];
+  
+  let slideIndex = 0;
+  
+  const createSlide = () => {
+    if (slideIndex >= movies.length) {
+      slideIndex = 0;
+    }
+  
+    let slide = document.createElement("div");
+    var imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+  
+    imgElement.appendChild(document.createTextNode(""));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+  
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+  
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    p.className = "movie-des";
+  
+    sliders.push(slide);
+  
+    if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+        30 * (sliders.length - 2)
+      }px)`;
+    }
+  };
+  for (let i = 0; i < 3; i++) {
+    createSlide();
+  }
+  setInterval(() => {
+    createSlide();
+  }, 5000);
+  
+  //video card
+  
+  const videoCards = [...document.querySelectorAll(".video-card")];
+  
+  videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      let video = item.children[1];
+      video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      let video = item.children[1];
+      video.pause();
+    });
+  });
+  
+  //card slider
+  
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+  
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+  
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
+  
